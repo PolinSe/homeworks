2dd0779dd162ba35dc29f3f14b628df01a379ba9
Polina Sedunova <lina.malina.spb@gmail.com>
2022-07-30 16:48:42 +0300

    feat: finish hw4

--- src/p2-homeworks/h4/HW4.tsx
+++ src/p2-homeworks/h4/HW4.tsx
@@ -30,11 +30,12 @@ function HW4() {
                     onChangeText={setText}
                     onEnter={showAlert}
                     error={error}
-                    // spanClassName={s.testSpanError}
+                    spanClassName={s.testSpanError}
                 />
 
                 <SuperInputText
-                    className={s.blue} // проверьте, рабоет ли смешивание классов
+                    className={s.blue} // проверьте, работает ли смешивание классов
+                    spanClassName={s.testSpanError}
                 />
 
                 {/*----------------------------------------------------*/}
